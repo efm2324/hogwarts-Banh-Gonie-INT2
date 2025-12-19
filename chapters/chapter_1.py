ca4a09ec6d95278e0db76b4e4db38bb40ca34b9b
efm2324 <gonie.louis@gmail.com>
2025-12-19 10:09:29 +0100

    import added

--- chapters/chapter_1.py
+++ chapters/chapter_1.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from utils.input_utils import load_file, ask_number, ask_choice
 
 # Add the parent directory to sys.path so we can import utils (doesn't work without this)
 sys.path.insert(0, str(Path(__file__).parent.parent))
