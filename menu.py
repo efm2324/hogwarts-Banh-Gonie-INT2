81dcb6c08a01f7f21c0d2fbe21c2b65249f626d5
Akitsuuuuu <kenny.banh.ly@gmail.com>
2026-01-03 16:57:14 +0100

    Diplay menu done

--- menu.py
+++ menu.py
@@ -1,1 +1,13 @@
 
+def display_main_menu():
+    print("1. Start Chapter 1 - Arrival in the magic world")
+    choice =int(input("2. Exit Game"))
+    if choice == 2:
+        print("Exiting the game.)
+        exit(0)
+    if choice == 1:
+        from chapters.chapter_1 import start_chater_1
+        character = start_chater_1()
+        return character
+
+
